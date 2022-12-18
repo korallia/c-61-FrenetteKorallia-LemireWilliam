0054c938e53d08170a8657f9093d57dc7d477513
Korallia Frenette <koralliafrenette@gmail.com>
2022-12-18 08:16:09 -0500

    Fix user for finance

--- c-61/sprint1/TrocQc/src/main/java/com/TrocQc/DAO/FinanceDao.java
+++ c-61/sprint1/TrocQc/src/main/java/com/TrocQc/DAO/FinanceDao.java
@@ -16,55 +16,44 @@ import com.TrocQc.config.SpringJdbcConfig;
 
 public class FinanceDao extends SpringJdbcConfig {
 
-	public FinanceDao() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public List<Vente> GetAllVentes(){
-		
-		Map<String, String> params = new HashMap<>();
-		List<Vente> ventes = namedParameterJdbcTemplate().query("select * from vente ORDER BY id", params,
-				BeanPropertyRowMapper.newInstance(Vente.class)); 
-		return ventes; 
-		
-		
-	}
-		
-	
-	public LinkedList<Point> GetDailySales(){
-		try {
+	private int UserId;
 
-			Map<String, String> params = new HashMap<>();
-			// +1 since we want to count the first day as day #1
-			List<Point> points = namedParameterJdbcTemplate().query("select (DATEDIFF(ventedate, (SELECT min(ventedate)  from vente) )+1) x, sum(montant) y  from vente group by DATE(ventedate) ORDER BY ventedate", params,
-					BeanPropertyRowMapper.newInstance(Point.class));
-
-			
-			LinkedList<Point> ll = new LinkedList<Point>();
-			for(int i = 0; i<points.size(); i++) {
-				ll.add(points.get(i));
-			}
-			return ll;
-
-		} catch (Exception e) {
-			return null;
-		}
+	public FinanceDao(int UserId) {
+		this.UserId = UserId;
 	}
-	
-	public LinkedList<Point> GetDailySalesOfPeriod(Date start, Date end){
+
+	public int getUserId() {
+		return UserId;
+	}
+
+	public void setUserId(int userId) {
+		UserId = userId;
+	}
+
+	public List<Vente> GetAllVentes() {
+
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("userid", this.UserId);
+
+		List<Vente> ventes = namedParameterJdbcTemplate().query("select * from vente WHERE userid=:userid ORDER BY id",
+				params, BeanPropertyRowMapper.newInstance(Vente.class));
+		return ventes;
+
+	}
+
+	public LinkedList<Point> GetDailySales() {
 		try {
 
 			MapSqlParameterSource params = new MapSqlParameterSource();
-			params.addValue("start", start.toString());
-			params.addValue("end", end.toString());
-			
-			// +1 since we want to count the first day as day #1
-			List<Point> points = namedParameterJdbcTemplate().query("select (DATEDIFF(ventedate, :start)+1) x, sum(montant) y  from vente WHERE ventedate>=:start AND ventedate<=:end  group by DATE(ventedate)  ORDER BY ventedate", params,
-					BeanPropertyRowMapper.newInstance(Point.class));
+			params.addValue("userid", this.UserId);
 
-			
+			// +1 since we want to count the first day as day #1
+			List<Point> points = namedParameterJdbcTemplate().query(
+					"select (DATEDIFF(ventedate, (SELECT min(ventedate)  from vente) )+1) x, sum(montant) y  from vente WHERE userid=:userid GROUP BY DATE(ventedate) ORDER BY ventedate",
+					params, BeanPropertyRowMapper.newInstance(Point.class));
+
 			LinkedList<Point> ll = new LinkedList<Point>();
-			for(int i = 0; i<points.size(); i++) {
+			for (int i = 0; i < points.size(); i++) {
 				ll.add(points.get(i));
 			}
 			return ll;
@@ -74,20 +63,45 @@ public class FinanceDao extends SpringJdbcConfig {
 		}
 	}
 
-	public LinkedList<DatePoint> GetDailySalesOfPeriodByDay(Date start, Date end){
+	public LinkedList<Point> GetDailySalesOfPeriod(Date start, Date end) {
 		try {
 
 			MapSqlParameterSource params = new MapSqlParameterSource();
 			params.addValue("start", start.toString());
 			params.addValue("end", end.toString());
-			
-			// +1 since we want to count the first day as day #1
-			List<DatePoint> points = namedParameterJdbcTemplate().query("select ventedate date, sum(montant) value  from vente WHERE ventedate>=:start AND ventedate<=:end  group by DATE(ventedate)  ORDER BY ventedate", params,
-					BeanPropertyRowMapper.newInstance(DatePoint.class));
+			params.addValue("userid", this.UserId);
 
-			
+			// +1 since we want to count the first day as day #1
+			List<Point> points = namedParameterJdbcTemplate().query(
+					"select (DATEDIFF(ventedate, :start)+1) x, sum(montant) y  from vente WHERE userid=:userid AND ventedate>=:start AND ventedate<=:end  group by DATE(ventedate)  ORDER BY ventedate",
+					params, BeanPropertyRowMapper.newInstance(Point.class));
+
+			LinkedList<Point> ll = new LinkedList<Point>();
+			for (int i = 0; i < points.size(); i++) {
+				ll.add(points.get(i));
+			}
+			return ll;
+
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public LinkedList<DatePoint> GetDailySalesOfPeriodByDay(Date start, Date end) {
+		try {
+
+			MapSqlParameterSource params = new MapSqlParameterSource();
+			params.addValue("start", start.toString());
+			params.addValue("end", end.toString());
+			params.addValue("userid", this.UserId);
+
+			// +1 since we want to count the first day as day #1
+			List<DatePoint> points = namedParameterJdbcTemplate().query(
+					"select ventedate date, sum(montant) value  from vente WHERE userid=:userid AND ventedate>=:start AND ventedate<=:end  group by DATE(ventedate)  ORDER BY ventedate",
+					params, BeanPropertyRowMapper.newInstance(DatePoint.class));
+
 			LinkedList<DatePoint> ll = new LinkedList<>();
-			for(int i = 0; i<points.size(); i++) {
+			for (int i = 0; i < points.size(); i++) {
 				ll.add(points.get(i));
 			}
 			return ll;
